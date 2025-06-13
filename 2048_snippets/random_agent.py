b632e4681bf777770232e3639372e93304930e29
Edd022 <garciajulian258rgmail.com>
2025-06-13 00:41:04 -0500

    w3 upload

--- 2048_snippets/random_agent.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class RandomAgent:
-    def __init__(self, action_space):
-        self.action_space = action_space
-
-    def select_action(self, observation):
-        return random.choice(self.action_space)
